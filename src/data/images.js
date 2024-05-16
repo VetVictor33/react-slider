0c80620f0f5e51a7bb022b9960c99c33524205b2
Victor Feliciano <victorjfeliciano@gmail.com>
2024-05-16 16:29:56 -0300

    refactor: replaced images with better resolution ones

--- src/data/images.js
+++ src/data/images.js
@@ -1,22 +1,22 @@
 export const images = [
   {
-    url: "https://picsum.photos/id/0/367/267",
+    url: "https://fastly.picsum.photos/id/55/4608/3072.jpg?hmac=ahGhylwdN52ULB37deeMZX6T_G7NiERtoPhwydMvUKQ",
     alt: "Image 1",
-    link: "https://picsum.photos/id/0/367/267",
+    link: "https://fastly.picsum.photos/id/55/4608/3072.jpg?hmac=ahGhylwdN52ULB37deeMZX6T_G7NiERtoPhwydMvUKQ",
   },
   {
-    url: "https://picsum.photos/id/12/367/267",
+    url: "https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s",
     alt: "Image 2",
-    link: "https://picsum.photos/id/12/367/267",
+    link: "https://fastly.picsum.photos/id/13/2500/1667.jpg?hmac=SoX9UoHhN8HyklRA4A3vcCWJMVtiBXUg0W4ljWTor7s",
   },
   {
-    url: "https://picsum.photos/id/22/367/267",
+    url: "https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU",
     alt: "Image 3",
-    link: "https://picsum.photos/id/22/367/267",
+    link: "https://fastly.picsum.photos/id/29/4000/2670.jpg?hmac=rCbRAl24FzrSzwlR5tL-Aqzyu5tX_PA95VJtnUXegGU",
   },
   {
-    url: "https://picsum.photos/id/33/367/267",
+    url: "https://fastly.picsum.photos/id/37/2000/1333.jpg?hmac=vpYLNsQZwU2szsZc4Uo17cW786vR0GEUVq4icaKopQI",
     alt: "Image 4",
-    link: "https://picsum.photos/id/33/367/267",
+    link: "https://fastly.picsum.photos/id/37/2000/1333.jpg?hmac=vpYLNsQZwU2szsZc4Uo17cW786vR0GEUVq4icaKopQI",
   },
 ];
